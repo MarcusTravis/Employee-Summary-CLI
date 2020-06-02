5e0de2b3954857e9654241118fa7e0847c7d21d4
Marcus <mpt789@gmail.com>
2020-06-01 19:07:32 -0700

    added changes to all employee templates and finished running all tests with all tests passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Employee class
+// //The first class is an Employee parent class with the following properties and methods:
+
+// name
+// id
+// email
+// getName()
+// getId()
+// getEmail()
+// getRole() // Returns 'Employee'
+
+class Employee {
+    constructor(name, id, email){
+        //   * name
+        this.name = name;
+        //   * id
+        this.id = id;
+        //   * email
+        this.email = email;
+    }
+      getName() {
+        return this.name;
+      }
+      getId() {
+        return this.id;
+      }
+      getEmail() {
+        return this.email;
+      }
+      getRole() {
+        return "Employee";
+      }
+}
+module.exports = Employee;
